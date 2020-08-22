85cbf146b927d451804ca1caa97162a670f2f8b2
Duc Tran <ducvtrann@gmail.com>
2020-08-21 21:41:50 -0400

    Update file path for express

--- index.js
+++ index.js
@@ -59,10 +59,10 @@ io.on('connection', (socket) => {
   });
 });
 
-app.use(express.static(path.join(__dirname, '../', 'client', 'build')));
+app.use(express.static(path.join(__dirname, 'client', 'build')));
 
 app.get('*', function (req, res) {
-  res.sendFile(path.join(__dirname, '../', 'client', 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
 });
 app.use(cors());
 
